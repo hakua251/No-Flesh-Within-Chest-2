ce51552dd5da5a8a323e55c32a8c3976e79cca9e
Neha YoruNina <836193631@qq.com>
2025-05-11 15:14:19 +0800

    建立物品凋亡映射

--- kubejs/startup_scripts/block/vita_random_decay.js
+++ kubejs/startup_scripts/block/vita_random_decay.js
@@ -1,1 +1,27 @@
 // priority: 900
+/**
+ * @type {Map<Internal.Block_, Internal.BlockState_>}
+ */
+const VitaBlockRandomDecayMap = new Map()
+
+BlockEvents.modification((event) => {
+    VitaBlockRandomDecayMap.forEach((v, k) => {
+        event.modify(k, ctx => {
+            ctx.setRandomTickCallback(/** @param {Internal.RandomTickCallbackJS} pCtx**/pCtx => {
+                const level = pCtx.level
+                if (level.getDimension().toString()!='minecraft:overworld') return
+                level.setBlockAndUpdate(pCtx.block.pos, v)
+            })
+        })
+    })
+})
+
+/**
+ * 
+ * @param {Internal.BlockPredicate_} block 
+ * @param {Internal.BlockState_} targetBlock 
+ */
+function RegisterVitaBlockRandomDecay(block, targetBlock) {
+    VitaBlockRandomDecayMap.set(block, targetBlock)
+}
+
