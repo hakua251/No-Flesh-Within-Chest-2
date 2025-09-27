ecea5db0be1949510aa12f948976a61353a95c4a
Neha YoruNina <836193631@qq.com>
2025-09-27 17:30:32 +0800

    基本胸腔类型定义

--- kubejs/server_scripts/domain/common/item_in_nether_portal.js
+++ kubejs/server_scripts/domain/common/item_in_nether_portal.js
@@ -4,13 +4,14 @@ InfinityEvents.itemInNetherPortal(event => {
     const itemStack = itemEntity.getItem()
     const level = event.getLevel()
     const pos = event.getPos()
-    let nbt = itemStack.getNbt()
 
-    if (itemStack.is('minecraft:gold_ingot')) {
-        let isSucc = InfinityPortalCreator.modifyOnInitialCollision('infinity:gold_cave', level, pos)
-        if (isSucc) itemEntity.remove('changed_dimension')
-    } else if (itemStack.is('minecraft:diamond')) {
-        let isSucc = InfinityPortalCreator.modifyOnInitialCollision('infinity:diamond_cave', level, pos)
+    if (itemStack.is('kubejs:key_to_infinity')) {
+        let itemName = itemStack.getHoverName()
+        let nameString = itemName.getString()
+        if (!nameString.startsWith('infinity:')) {
+            nameString = 'infinity:'.concat(nameString.trim())
+        }
+        let isSucc = InfinityPortalCreator.modifyOnInitialCollision(nameString, level, pos)
         if (isSucc) itemEntity.remove('changed_dimension')
     }
 })
