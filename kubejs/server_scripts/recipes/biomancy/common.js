bcc3a9cfc77c9dafce66bfc57e129ef3b1842574
Neha YoruNina <836193631@qq.com>
2025-12-18 20:27:34 +0800

    更换背包整理模组，开启胸腔认知线

---
+++ kubejs/server_scripts/recipes/biomancy/common.js
@@ -0,0 +1,6 @@
+// priority: 1000
+ServerEvents.recipes(event => {
+    event.shapeless('biomancy:primordial_core', [
+        'chestcavity:rotten_heart', 'chestcavity:animal_heart', 'chestcavity:heart'
+    ])
+})
