0fdcf402fd6cadc1049d587b56061027a6e9e6ac
Neha YoruNina <836193631@qq.com>
2026-01-07 02:46:07 +0800

    罐装美味 #1

--- kubejs/server_scripts/recipes/biomancy/common.js
+++ kubejs/server_scripts/recipes/biomancy/common.js
@@ -3,4 +3,6 @@ ServerEvents.recipes(event => {
     event.shapeless('biomancy:primordial_core', [
         'chestcavity:rotten_heart', 'chestcavity:animal_heart', 'chestcavity:heart'
     ])
+
+    event.recipes.biomancy.bio_brewing(['minecraft:redstone', 'biomancy:genetic_compound', 'biomancy:decaying_additive', 'minecraft:glowstone_dust'], 'kubejs:jar_of_vacuum', 'kubejs:jar_of_mystery')
 })
