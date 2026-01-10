4ac01f73a50c87929e97edf5728751e70327032e
Neha YoruNina <836193631@qq.com>
2026-01-11 00:46:16 +0800

    支持食用任务类型

--- kubejs/startup_scripts/item/material.js
+++ kubejs/startup_scripts/item/material.js
@@ -1,4 +1,29 @@
 // priority: 1000
 StartupEvents.registry('item', event => {
     event.create('sawdust').texture('kubejs:item/material/sawdust').maxStackSize(64).burnTime(400)
+
+    
+    event.create('kubejs:dried_taste_worm').maxStackSize(1).food(food => food.hunger(1).saturation(1)).texture('kubejs:item/food/dried_taste_worm')
+
+    event.create('kubejs:fried_taste_worm').maxStackSize(1).food(food => food.hunger(2).saturation(1)).texture('kubejs:item/food/fried_taste_worm')
+    
+    event.create('kubejs:simple_cooked_taste_worm').maxStackSize(1).food(food => food.hunger(2).saturation(0.5)).texture('kubejs:item/food/simple_cooked_taste_worm')
+
+    event.create('kubejs:taste_worm_powder').maxStackSize(1).food(food => food.hunger(1).saturation(1)).texture('kubejs:item/food/taste_worm_powder')
+
+    event.create('kubejs:taste_worm_meat_filling').maxStackSize(1).food(food => food.hunger(1).saturation(1)).texture('kubejs:item/food/taste_worm_meat_filling')
+
+    event.create('kubejs:taste_worm_meat_patty').maxStackSize(1).food(food => food.hunger(3).saturation(1)).texture('kubejs:item/food/taste_worm_meat_patty')
+
+    event.create('kubejs:fried_taste_worm_meat_patty').maxStackSize(1).food(food => food.hunger(6).saturation(2).effect('minecraft:fire_resistance', 1200, 0, 1)).texture('kubejs:item/food/fried_taste_worm_meat_patty')
+
+    event.create('kubejs:tasty_dough').maxStackSize(1).food(food => food.hunger(4).saturation(1)).texture('kubejs:item/food/tasty_dough')
+
+    event.create('kubejs:tasty_bread').maxStackSize(1).food(food => food.hunger(8).saturation(0.5).effect('minecraft:haste', 1200, 0, 1)).texture('kubejs:item/food/tasty_bread')
+
+    event.create('kubejs:tasty_beer').maxStackSize(1).food(food => food.hunger(2).saturation(0.5).effect('brewery:drunk', 600, 2, 1)).texture('kubejs:item/food/tasty_beer')
+
+    event.create('kubejs:tasty_worm_soup').maxStackSize(1).food(food => food.hunger(6).saturation(1.5).effect('minecraft:haste', 1200, 0, 1)).texture('kubejs:item/food/tasty_worm_soup')
+
+    event.create('kubejs:tasty_worm_meat_bun').maxStackSize(1).food(food => food.hunger(4).saturation(1).effect('minecraft:haste', 1200, 0, 1)).texture('kubejs:item/food/tasty_worm_meat_bun')
 })
--- kubejs/startup_scripts/item/organ/food.js
+++ kubejs/startup_scripts/item/organ/food.js
@@ -24,27 +24,4 @@ StartupEvents.registry('item', event => {
 
     event.create('kubejs:worm_of_taste').maxStackSize(1).food(food => food.hunger(1).saturation(1).effect('minecraft:nausea', 200, 0, 1)).texture('kubejs:item/organs/food/worm_of_taste').tag('kubejs:food').tag('kubejs:infected')
 
-    event.create('kubejs:dried_taste_worm').maxStackSize(1).food(food => food.hunger(1).saturation(1)).texture('kubejs:item/organs/food/dried_taste_worm').tag('kubejs:food')
-
-    event.create('kubejs:fried_taste_worm').maxStackSize(1).food(food => food.hunger(2).saturation(1)).texture('kubejs:item/organs/food/fried_taste_worm').tag('kubejs:food')
-    
-    event.create('kubejs:simple_cooked_taste_worm').maxStackSize(1).food(food => food.hunger(2).saturation(0.5)).texture('kubejs:item/organs/food/simple_cooked_taste_worm').tag('kubejs:food')
-
-    event.create('kubejs:taste_worm_powder').maxStackSize(1).food(food => food.hunger(1).saturation(1)).texture('kubejs:item/organs/food/taste_worm_powder').tag('kubejs:food')
-
-    event.create('kubejs:taste_worm_meat_filling').maxStackSize(1).food(food => food.hunger(1).saturation(1)).texture('kubejs:item/organs/food/taste_worm_meat_filling').tag('kubejs:food')
-
-    event.create('kubejs:taste_worm_meat_patty').maxStackSize(1).food(food => food.hunger(3).saturation(1)).texture('kubejs:item/organs/food/taste_worm_meat_patty').tag('kubejs:food')
-
-    event.create('kubejs:fried_taste_worm_meat_patty').maxStackSize(1).food(food => food.hunger(6).saturation(2).effect('minecraft:fire_resistance', 1200, 0, 1)).texture('kubejs:item/organs/food/fried_taste_worm_meat_patty').tag('kubejs:food')
-
-    event.create('kubejs:tasty_dough').maxStackSize(1).food(food => food.hunger(4).saturation(1)).texture('kubejs:item/organs/food/tasty_dough').tag('kubejs:food')
-
-    event.create('kubejs:tasty_bread').maxStackSize(1).food(food => food.hunger(8).saturation(0.5).effect('minecraft:haste', 1200, 0, 1)).texture('kubejs:item/organs/food/tasty_bread').tag('kubejs:food')
-
-    event.create('kubejs:tasty_beer').maxStackSize(1).food(food => food.hunger(2).saturation(0.5).effect('brewery:drunk', 600, 2, 1)).texture('kubejs:item/organs/food/tasty_beer').tag('kubejs:food')
-
-    event.create('kubejs:tasty_worm_soup').maxStackSize(1).food(food => food.hunger(6).saturation(1.5).effect('minecraft:haste', 1200, 0, 1)).texture('kubejs:item/organs/food/tasty_worm_soup').tag('kubejs:food')
-
-    event.create('kubejs:tasty_worm_meat_bun').maxStackSize(1).food(food => food.hunger(4).saturation(1).effect('minecraft:haste', 1200, 0, 1)).texture('kubejs:item/organs/food/tasty_worm_meat_bun').tag('kubejs:food')
 })
