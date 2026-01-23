2ba10a7db0398184fb89ea6f6308bd33a57fdb13
Neha YoruNina <836193631@qq.com>
2026-01-23 18:17:40 +0800

    修复LeaderBoard任务异常

--- kubejs/server_scripts/domain/special/dimension/oath.js
+++ kubejs/server_scripts/domain/special/dimension/oath.js
@@ -1,10 +1,10 @@
 // priority: 500
 // todo 可能更换生成方式
-LevelEvents.loaded('kubejs:oath', event => {
-    if (AStages.serverHasStage('gen_oath', event.server)) return
-    /**@type {Internal.ServerLevel} */
-    const level = event.level
-    GenerateStructureByCenter(level, new BlockPos(0, 0, 0), 'kubejs:oath')
-    level.setDefaultSpawnPos(new BlockPos(0, 1, 0), 0)
-    AStages.addStageToServer('gen_oath', event.server)
-})
+// LevelEvents.loaded('kubejs:oath', event => {
+//     if (AStages.serverHasStage('gen_oath', event.server)) return
+//     /**@type {Internal.ServerLevel} */
+//     const level = event.level
+//     GenerateStructureByCenter(level, new BlockPos(0, 0, 0), 'kubejs:oath')
+//     level.setDefaultSpawnPos(new BlockPos(0, 1, 0), 0)
+//     AStages.addStageToServer('gen_oath', event.server)
+// })
