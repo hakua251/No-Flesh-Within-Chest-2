54f05fa19ec430084c9020c171aa937f80667358
Neha YoruNina <836193631@qq.com>
2026-06-11 03:03:12 +0800

    Qol改良

--- kubejs/server_scripts/recipes/machine/growth_vat/mutation_config/rose_organ.js
+++ kubejs/server_scripts/recipes/machine/growth_vat/mutation_config/rose_organ.js
@@ -19,7 +19,7 @@ RegistryTumorMutationConfig(RoseQuartzHeartMutationConfigModel)
 
 const RoseQuartzRibMutationConfigModel = new TumorMutationConfigModel()
     .setCondition((machine, fluid, item, slotId, organData) => {
-        return fluid.id == 'kubejs:rose_nutrients_fluid' && organData.getFloat('kubejs:rosy') >= 1.0 && organData.getFloat('chestcavity:health') >= 2.0
+        return fluid.id == 'kubejs:rose_nutrients_fluid' && organData.getFloat('kubejs:rosy') >= 1.0 && organData.getFloat('chestcavity:defense') >= 2.0
     })
     .setResult((machine, fluid, item, slotId, organData) => {
         return Item.of('kubejs:rose_quartz_rib')
